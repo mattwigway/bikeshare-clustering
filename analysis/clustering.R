5b0b9415ec66c0c31d4d98b97f0ba69daaff73f5
Matt Conway <mattwigway@gmail.com>
2014-04-20 21:04:48 -0700

    First working example.

---
+++ analysis/clustering.R
@@ -0,0 +1,73 @@
+library(plyr)
+
+tripdata <- read.csv('tripdata.csv')
+summary(tripdata)
+
+# maximum number of clusters
+maxk <- 10
+
+tot.withinss <- rep(NA, maxk)
+
+for (k in 2:maxk) {
+  # perform the clustering by accessibility ratios. No need to rescale as these are already unitless
+  km <- kmeans(tripdata[,c('jobs10', 'jobs30', 'jobs60', 'population10', 'population30', 'population60', 'bike30')],
+         k,nstart=20)
+  tot.withinss[k] <- km$tot.withinss
+}
+
+# 6 looks pretty good
+km <- kmeans(tripdata[,c('jobs10', 'jobs30', 'jobs60', 'population10', 'population30', 'population60', 'bike30')],
+             6,nstart=20)
+
+tripdata$cluster <- km$cluster
+
+# summarize the data
+data <- ddply(tripdata, c('cluster'), summarise,
+      mean.jobs10=mean(jobs10),
+      sd.jobs10=sd(jobs10),
+      conf.jobs10=sd(jobs10)/sqrt(length(jobs10)) * qt(.975, length(jobs10) - 1),
+      n.jobs10=length(jobs10),
+      
+      mean.jobs30=mean(jobs30),
+      sd.jobs30=sd(jobs30),
+      conf.jobs30=sd(jobs30)/sqrt(length(jobs30)) * qt(.975, length(jobs30) - 1),
+      n.jobs30=length(jobs30),
+      
+      mean.jobs60=mean(jobs60),
+      sd.jobs60=sd(jobs60),
+      conf.jobs60=sd(jobs60)/sqrt(length(jobs60)) * qt(.975, length(jobs60) - 1),
+      n.jobs60=length(jobs60),
+      
+      mean.population10=mean(population10),
+      sd.population10=sd(population10),
+      conf.population10=sd(population10)/sqrt(length(population10)) * qt(.975, length(population10) - 1),
+      n.population10=length(population10),
+      
+      mean.population30=mean(population30),
+      sd.population30=sd(population30),
+      conf.population30=sd(population30)/sqrt(length(population30)) * qt(.975, length(population30) - 1),
+      n.population30=length(population30),
+      
+      mean.population60=mean(population60),
+      sd.population60=sd(population60),
+      conf.population60=sd(population60)/sqrt(length(population60)) * qt(.975, length(population60) - 1),
+      n.population60=length(population60),
+      
+      mean.bike30=mean(bike30),
+      sd.bike30=sd(bike30),
+      conf.bike30=sd(bike30)/sqrt(length(bike30)) * qt(.975, length(bike30) - 1),
+      n.bike30=length(bike30),
+      
+      p.weekend=sum(weekend=='True')/length(weekend),
+      # 1.96: two-tailed critical value for normal distribution
+      conf.weekend=sqrt(p.weekend * (1-p.weekend)/length(weekend))*1.96,
+      n.weekend=length(weekend),
+      
+      p.casual=sum(casual=='True')/length(casual),
+      # 1.96: two-tailed critical value for normal distribution
+      conf.casual=sqrt(p.casual * (1-p.casual)/length(casual))*1.96,
+      n.casual=length(casual)
+)
+
+# save the data for visualization
+write.csv(data, 'data.csv')
---
+++ chart.js
@@ -0,0 +1,209 @@
+var width = 400;
+var height = 275;
+
+ // TODO: hardcoding is bad
+var ydiff = d3.scale.linear().domain([0, 14]).range([height,0]);
+var yp    = d3.scale.linear().domain([0, 0.5]).range([height,0]);
+
+var dragging = false;
+var dragx = null;
+
+var color = {
+    // we keep the same type of accessibility in the same color
+    jobs10: d3.rgb('#41417b'),
+    jobs30: d3.rgb('#41417b'),
+    jobs60: d3.rgb('#41417b'),
+
+    population10: d3.rgb('#22571e'),
+    population30: d3.rgb('#22571e'),
+    population60: d3.rgb('#22571e'),
+
+    bike30: d3.rgb('#56706c'),
+
+    // both of the percentages: casual and weekend use
+    // note that this is in a warm color to make it appear quantitatively different from the others,
+    // which is because all of the others were used in the clustering but these variables were not
+    casual: d3.rgb('#9d4343'),
+    weekend: d3.rgb('#9d4343')
+};
+
+d3.csv('data.csv', function (data) {
+    // debugging
+    theData = data;
+
+    var chartArea = d3.select('#viz');
+
+    // draw the same plot for each of the clusters
+    data.forEach(function (cluster, clusterIndex) {
+        var chart = chartArea.append('div')
+            .attr('class', 'cluster-chart')
+            .append('svg')
+            .attr('width', width + 'px')
+            .attr('height', (height + 50) + 'px')
+            .append('g');
+
+        // add the bars separately
+        var vars = ['jobs10', 'jobs30', 'jobs60', 'population10', 'population30', 'population60', 'bike30', 'weekend', 'casual'];
+        var varLabels = ['10 min', '30 min', '60 min', '10 min', '30 min', '60 min', '30 min', 'Wknd', 'Casual'];
+        chart.selectAll('rect.bar')
+            .data(vars)
+            .enter().append('rect')
+            .attr('class', 'bar')
+            .attr('width', width / 10)
+            .attr('height', function (d) {
+                if (d == 'casual' || d == 'weekend')
+                    // p stands for probability, as in the binomial distribution
+                    return height - yp(cluster['p.' + d]);
+                else
+                    return height - ydiff(cluster['mean.' + d]);
+            })
+            .attr('transform', function (d, i) {
+                var x = i * width / 9;
+                if (d == 'casual' || d == 'weekend')
+                    var y = yp(cluster['p.' + d]);
+                else
+                    var y = ydiff(cluster['mean.' + d]);
+                return 'translate(' + x + ' ' + y + ')';
+            })
+            .attr('fill', function (d) { return color[d] });
+
+        // add confidence bands
+        chart.selectAll('line.band')
+            .data(vars)
+            .enter().append('line')
+            .attr('class', 'band')
+            .attr('x1', function (d, i) { return i * width / 9 + width/18 })
+            .attr('x2', function (d, i) { return i * width / 9 + width/18 })
+            // lower end
+            .attr('y1', function (d) {
+                if (d == 'casual' || d == 'weekend')
+                    return yp(Number(cluster['p.' + d]) - Number(cluster['conf.' + d]));
+                else
+                    return ydiff(Number(cluster['mean.' + d]) - Number(cluster['conf.' + d]));
+            })
+            // upper end
+            .attr('y2', function (d) {
+                if (d == 'casual' || d == 'weekend')
+                    return yp(Number(cluster['p.' + d]) + Number(cluster['conf.' + d]));
+                else
+                    return ydiff(Number(cluster['mean.' + d]) + Number(cluster['conf.' + d]));
+            })
+            .attr('stroke', '#000');       
+
+        // indifference line: accessibility at origin == accessibility at destination
+        chart.append('line')
+            .attr('x1', 0).attr('x2', 7 * width / 9 - (width / 9 - width / 10))
+            .attr('y1', ydiff(1)).attr('y2', ydiff(1))
+            .attr('stroke', '#000')
+            .attr('stroke-width', .75);
+
+        // add labels
+        var t = chart.selectAll('text.chLabel')
+            .data(vars)
+            .enter().append('text')
+            .attr('x', 15).attr('y', 15);
+            
+        t.append('tspan')
+            .text(function (d) {
+                return 'Cluster ' + (clusterIndex + 1);
+            });
+
+        t.append('tspan')
+            .attr('dy', 15).attr('x', 15)
+            .text('n: ' + cluster['n.jobs10']);
+
+        // now, all the fancy labels
+        chart.selectAll('text.colLabel')
+            .data(varLabels)
+            .enter().append('text')
+            .attr('class', 'colLabels')
+            .attr('x', function (d, i) {
+                return i * width / 9
+            })
+            .attr('y', height + 15)
+            .text(String);
+
+        chart.append('line')
+            .attr('x1', 0)
+            // leave a gap
+            .attr('x2', 3 * width / 9 - (width / 9 - width / 10))
+            .attr('y1', height + 20)
+            .attr('y2', height + 20)
+            .attr('stroke', '#000');
+
+        chart.append('line')
+            .attr('x1', 3 * width / 9)
+            // leave a gap
+            .attr('x2', 6 * width / 9 - (width / 9 - width / 10))
+            .attr('y1', height + 20)
+            .attr('y2', height + 20)
+            .attr('stroke', '#000');
+
+        chart.append('text')
+            .attr('x', 20)
+            .attr('y', height + 38)
+            .text('Jobs');
+
+        chart.append('text')
+            .attr('x', 3 * width / 9 + 20)
+            .attr('y', height + 38)
+            .text('Population');
+
+        var bSt = chart.append('text')
+            .attr('x', 6 * width / 9)
+            .attr('y', height + 33);
+
+        bSt.append('tspan')
+            .text('Bike');
+        bSt.append('tspan')
+            .attr('dy', 15)
+            .attr('x', 6 * width / 9)
+            .text('Stations')
+
+        
+                
+                
+    });
+
+    // set up dragging
+    chartArea.on('mousedown', function () {
+        dragging = true;
+    });
+
+    chartArea.on('mousemove', function () {
+        if (dragging) {
+            if (dragx != null)
+                // scroll by the amount moved
+                chartArea[0][0].scrollLeft -= d3.event.clientX - dragx;
+            dragx = d3.event.clientX;
+        }            
+    });
+    
+    chartArea.on('mouseup', function () {
+        dragging = false;
+        dragx = null;
+    });
+
+    // draw the axes
+    var paxis = d3.svg.axis()
+        .scale(yp)
+        .orient('right')
+        .tickFormat(d3.format('p'));
+
+    var diffaxis = d3.svg.axis()
+        .scale(ydiff)
+        .orient('left');
+
+    d3.selectAll('.axis')
+        .append('svg')
+        .attr('width', '40px')
+        .attr('height', (height + 10) + 'px')
+        .append('g')
+        .data([diffaxis, paxis])
+        .each(function (axis) {
+            axis(d3.select(this));
+        });
+
+    d3.select('.axis svg g')
+        .attr('transform', 'translate(40 0)');
+});
